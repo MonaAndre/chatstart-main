ad10a32d3c0688f293d1c22e9736b3add896be7f
Rözlin Akkurt <rozlin.akkurt@hotmail.com>
2024-03-13 15:04:06 +0100

    Fixed chat bubbles for current user
    Co-authored-by: Mona <MonaAndre@users.noreply.github.com>
    Co-authored-by: MickeCardaba <freesideMike@users.noreply.github.com>

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ const createMessageBtn = document.getElementById("create-message-btn");
 const messageTextArea= document.getElementById("message-text");
 const messageContainer = document.querySelector('.chat-messages');
 
-createMessageBtn.addEventListener("click",async (ev)=>{
+createMessageBtn.addEventListener("click", async (ev)=>{
+    newMessage(ev);
+})
+messageTextArea.addEventListener("keypress", async (ev)=>{
+    if (ev.key === 'Enter'){
+    newMessage(ev);
+}})   
+
+
+ const newMessage = async (ev) => {
     ev.preventDefault()
    
     const rawResponse = await fetch('http://localhost:3000/newMessage',{
@@ -17,22 +26,34 @@ createMessageBtn.addEventListener("click",async (ev)=>{
     if(rawResponse.status == 204){
         //const content = await rawResponse.json();
         console.log(messageTextArea.value);
+        messageTextArea.value="";
     } else{
         console.log('response from database');
         // error.style.display = "block";
     } 
+    
+    getAllMessages();
+    
+}
 
-})
+
+
 
 async function getAllMessages() {
-    const response = await fetch('http://localhost:3000/allMessages')
+    const response = await fetch('http://localhost:3000/allMessages',{
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method:'GET',
+          credentials:'include'              
+
+    })
     console.log(response);
     const data = await response.json();
     console.log(data);
     // return data;
     //const name = data.userName;
-    const chatText = data[1].text;
-    console.log(chatText);
    // drawMessages();
 
    for(let i = 0; i<data.length; i++) {
@@ -40,17 +61,26 @@ async function getAllMessages() {
     messageHolder.classList = "message-box-holder";
     messageContainer.appendChild(messageHolder);
 
+    if (data[i].isCurrentUser==false){
     const messageSender = document.createElement('div');
     messageSender.innerHTML = data[i].userName;
     messageSender.classList = "message-sender";
     messageHolder.appendChild(messageSender);
+    }
     
     const messageBox = document.createElement('div');
     messageBox.innerHTML = data[i].text;
-    messageBox.classList = "message-box";
+    if (data[i].isCurrentUser) {
+        messageBox.classList = "message-box";
+    } else {
+    messageBox.classList = "message-box message-partner";
+    console.log(data[i].isCurrentUser);
+}
     messageHolder.appendChild(messageBox);
 
    }
+   
+   messageContainer.scrollTop = messageContainer.scrollHeight;
 }
 
 window.addEventListener('load', getAllMessages())
